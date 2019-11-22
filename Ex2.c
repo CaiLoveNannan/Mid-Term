d823f52d77b1890a86561e0546aac681fffe19fa
LoveNannan <caizhiyuangood@163.com>
2019-11-22 00:41:41 -0800

    added a Ex2.c file

---
+++ Ex2.c
@@ -0,0 +1,24 @@
+#include<stdio.h)
+int main()
+{
+    unsigned int n = 0,max;
+    int i = 0;
+    char ch;
+    while(1)
+    {
+        ch = getchar();
+        if('0'<=ch&&ch<='9')
+        {
+            n = n*10+ch-'0';
+        }
+        else
+        {
+            if(i==1||max<n)max=n;
+            i++;
+            n=0;
+        }
+        if(ch=='\n')break;
+    }
+    printf("The max is %u\n",max);
+return 0;
+}
